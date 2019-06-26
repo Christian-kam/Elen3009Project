fafb054e1b36e05749590ab13bf7452a88d676f0
Christian-kam <chriskam91@gmail.com>
2019-06-26 08:59:52 +0200

    Update main.cpp
    Latest version of main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include "GameView.h"
+/*! This is file contains main()
+  \author Catherine Honegger and Christian Kamwangala
+  \version 2.2
+  \date 06/09/2015
+*/
+#include "GameEngine.h"
 
 int main()
 {
-	GameView game;
-	game.run();
+    std::unique_ptr<GameEngine> _game{new GameEngine()};
+    _game->start();
 
+	return 0;
 }
